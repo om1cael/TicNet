b8a5cb507b76662db73f9440717936ca86344440
om1cael <188113439+om1cael@users.noreply.github.com>
2025-01-15 05:22:51 -0300

    feat: improve client input reading

--- src/main/java/com/om1cael/ticnet/network/Player.java
+++ src/main/java/com/om1cael/ticnet/network/Player.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Player implements Runnable {
     private final Socket socket;
@@ -28,27 +29,35 @@ public class Player implements Runnable {
     public void run() {
         log.info("New connection from {}", this.socket.getInetAddress());
 
-        while(this.isRunning) {
-            this.readClient();
-        }
-    }
-
-    private void readClient() {
         try(BufferedReader clientData = new BufferedReader(new InputStreamReader(this.socket.getInputStream()))) {
-            String input = clientData.readLine();
-            if(input == null || this.socket.isClosed()) {
-                this.disconnect();
-                return;
+            while(this.isRunning) {
+                this.readClient(clientData);
             }
-
-            this.parseClient(clientData.readLine());
         } catch (IOException e) {
             log.info("Could not read data from player id {} at {}",
                     this.id,
                     this.socket.getInetAddress(),
                     e
             );
+
+            if(e.getMessage().equalsIgnoreCase("Socket is closed")) {
+                this.disconnect();
+            }
         }
+    }
+
+    private void readClient(BufferedReader clientData) throws IOException {
+        String input = clientData.readLine();
+        if(input == null || this.socket.isClosed()) {
+            log.warn("Input is null or socket is closed, starting to disconnect {}! Input: {}",
+                    this.socket.getInetAddress(),
+                    input
+            );
+            this.disconnect();
+            return;
+        }
+
+        this.parseClient(input);
     }
 
     private void parseClient(String clientData) {
