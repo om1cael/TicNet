dbe599051179a158d402ff342a63f88da3d7e2c2
om1cael <188113439+om1cael@users.noreply.github.com>
2025-01-15 03:05:08 -0300

    feat: setup server socket

--- src/main/java/com/om1cael/ticnet/Main.java
+++ src/main/java/com/om1cael/ticnet/Main.java
@@ -1,7 +1,29 @@
 package com.om1cael.ticnet;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main {
+    private final short SERVER_PORT = 1024;
+
+    private final Logger log = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        new Main().start();
+    }
+
+    private void start() {
+        try(ServerSocket serverSocket = new ServerSocket(this.SERVER_PORT)) {
+            log.info("Server started at port {}", this.SERVER_PORT);
+
+            while(true) {
+                // Create client thread
+            }
+        } catch (IOException e) {
+            log.fatal("Server could not be started!", e);
+        }
     }
 }
