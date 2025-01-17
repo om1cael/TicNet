fefd8dc1a07d0c5c8c92de7e2ed6a7a02cf77cb8
om1cael <188113439+om1cael@users.noreply.github.com>
2025-01-16 23:56:27 -0300

    feat: add method for stopping a game

--- src/main/java/com/om1cael/ticnet/network/Game.java
+++ src/main/java/com/om1cael/ticnet/network/Game.java
@@ -1,14 +1,30 @@
 package com.om1cael.ticnet.network;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Game {
     private Player host;
     private Optional<Player> guest;
 
+    private AtomicBoolean isRunning;
+
     public Game(Player host, Optional<Player> guest) {
         this.host = host;
         this.guest = guest;
+        this.isRunning = new AtomicBoolean(false);
+    }
+
+    public void stopGame(boolean abruptStop) {
+        this.isRunning.set(false);
+
+        if(abruptStop) {
+            // Send message regarding abrupt stop
+            return;
+        }
+
+        // Send message regarding stop
+        // Anything else that needs to be done
     }
 
     public void setGuest(Optional<Player> guest) {
