2fd1c19bab32a584e5cd8ca72e44ec6bfc9f8372
om1cael <188113439+om1cael@users.noreply.github.com>
2025-01-17 17:14:06 -0300

    chore: add gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,4 +12,5 @@ repositories {
 dependencies {
     implementation(platform("org.apache.logging.log4j:log4j-bom:2.24.3"))
     implementation("org.apache.logging.log4j:log4j-core")
+    implementation("com.google.code.gson:gson:2.11.0")
 }
