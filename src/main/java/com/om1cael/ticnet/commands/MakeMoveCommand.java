bda7b41ecb8e48ed3a9e9b9b441ee24378ee35cf
om1cael <188113439+om1cael@users.noreply.github.com>
2025-01-18 19:31:27 -0300

    feat(make move): add logging of command errors

--- src/main/java/com/om1cael/ticnet/commands/MakeMoveCommand.java
+++ src/main/java/com/om1cael/ticnet/commands/MakeMoveCommand.java
@@ -3,6 +3,8 @@ package com.om1cael.ticnet.commands;
 import com.om1cael.ticnet.network.Game;
 import com.om1cael.ticnet.network.Player;
 import com.om1cael.ticnet.response.CommandResponses;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,6 +14,8 @@ public class MakeMoveCommand {
     private final Game game;
     private final String clientData;
 
+    private final Logger log = LogManager.getLogger(MakeMoveCommand.class);
+
     public MakeMoveCommand(Player player, Game game, String clientData) {
         this.player = player;
         this.game = game;
@@ -20,18 +24,21 @@ public class MakeMoveCommand {
 
     public void run() {
         if(game == null) {
+            log.info("Player {} could not make move because game was not found", this.player.getSocket().getInetAddress());
             this.player.writeClient(CommandResponses.MAKE_MOVE_GAME_NOT_FOUND);
             return;
         }
 
         String[] splitClientData = this.clientData.split(" ");
         if(splitClientData.length < 3) {
+            log.info("Player {} could not make move because it did not include row or column", this.player.getSocket().getInetAddress());
             this.player.writeClient(CommandResponses.MAKE_MOVE_INSUFFICIENT_ARGS);
             return;
         }
 
         List<Integer> rowAndColumn = this.getRowAndColumn(splitClientData);
         if(rowAndColumn.isEmpty()) {
+            log.info("Player {} could not make move because row or column weren't numbers", this.player.getSocket().getInetAddress());
             this.player.writeClient(CommandResponses.MAKE_MOVE_ARGS_NOT_NUMBER);
             return;
         }
